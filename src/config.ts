61850bb307f9fa7c2b1588727b2e236d63154064
Mathieu "ralmn" HIREL <contact.ralmn@gmail.com>
2021-09-09 20:44:45 +0200

    Load config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,39 @@
+import { stat as fsStat } from "fs/promises";
+
 interface IConfig {
     auth: {
         jwt_secret: string
     }
 }
 
-export let config : IConfig = {
-    auth : {
-        jwt_secret: "secret"
+export let config: IConfig = {
+    auth: {
+        jwt_secret: 'secret'
+    }
+};
+
+export async function loadConfig() {
+    const configPath = process.cwd() + '/config.json';
+
+    try {
+        const stats = await fsStat(configPath);
+        if (stats && stats.isFile()) {
+            config = require(configPath);
+        }
+    } catch (e) { }
+
+    recurseLoad(config, 'version_checker');
+}
+
+function recurseLoad(obj, path) {
+    for (let key in obj) {
+        let envKey = `${path}_${key}`;
+        if (typeof obj[key] === 'object') {
+            recurseLoad(obj[key], envKey);
+        } else {
+            if (process.env[envKey.toUpperCase()]) {
+                obj[key] = process.env[envKey.toUpperCase()];
+            }
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import "reflect-metadata";
 import * as express from "express";
-import {json as jsonBodyParser} from "body-parser";
+import { json as jsonBodyParser } from "body-parser";
 import { mainRouter } from "./routes";
 import { createConnection } from "typeorm";
+import { loadConfig } from "./config";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 9999;
 
@@ -11,8 +12,15 @@ const app = express();
 app.use(jsonBodyParser());
 app.use('/', mainRouter);
 
-createConnection();
 
-app.listen(PORT, () => {
-    console.log(`Listen on port ${PORT}`)
-});
+async function main() {
+    await loadConfig();
+
+    await createConnection();
+
+    app.listen(PORT, () => {
+        console.log(`Listen on port ${PORT}`)
+    });
+}
+
+main().then(() => {});
