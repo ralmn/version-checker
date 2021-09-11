5082bdb2ac08cbfbab1269fcc6218a5a73b10227
Mathieu "ralmn" HIREL <contact.ralmn@gmail.com>
2021-09-11 21:11:01 +0200

    Handle build front by server

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
-import {Router} from "express";
+import {Router, static as expressStatic} from "express";
 import { apiRouter } from "./api";
 
 export const mainRouter = Router();
 
 mainRouter.use('/api', apiRouter);
+
+mainRouter.use('/',expressStatic('./front'))
