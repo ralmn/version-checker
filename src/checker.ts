e95ddb72972084f274dc22551dc9179d980c1d66
Mathieu "ralmn" HIREL <contact.ralmn@gmail.com>
2021-09-12 21:28:49 +0200

    Remove init on checker script

--- src/checker.ts
+++ src/checker.ts
@@ -1,47 +1,11 @@
-import { versions } from "process";
-import { BeforeInsert, createConnection, getConnection } from "typeorm";
-import { CustomSoftware } from "./entity/CustomSoftware";
-import { GithubSoftware } from "./entity/Githubsoftware";
+import { createConnection } from "typeorm";
 import { Software } from "./entity/Software";
-
-
-async function init() {
-    let repository: any = getConnection().getRepository(GithubSoftware);
-
-
-    let repos = [
-        { name: "traefik", repo: "traefik/traefik" },
-        { name: "prometheus", repo: "prometheus/prometheus" },
-        { name: "grafana", repo: "grafana/grafana" },
-    ]
-
-    await repository.save(repos.map(r => {
-        let gRepo = new GithubSoftware();
-        gRepo.name = r.name;
-        gRepo.repository = r.repo;
-        return gRepo;
-    }));
-
-
-    repository = getConnection().getRepository(CustomSoftware);
-    let versions = [8, 11, 16, 17];
-
-    await repository.save(versions.map(r => {
-        let cRepo = new CustomSoftware();
-        cRepo.name = `OpenJdk ${r}`;
-        cRepo.file = 'OpenJDKScanner';
-        cRepo.data = { version: r };
-        return cRepo;
-    }));
-
-}
+ 
 
 async function main() {
     let connection = await createConnection();
 
     let repository = connection.getRepository(Software);
-
-    await init();
 
     let softs = await repository
         .createQueryBuilder()
