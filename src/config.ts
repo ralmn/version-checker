0befdf424c4568273e07ae4e8937a74fd4f8d313
Mathieu ralmn HIREL <contact.ralmn@gmail.com>
2021-11-18 23:03:10 +0100

    Read config from env

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ interface IConfig {
     },
     dockerhub?: {
         user: string,
-        access_token: string
+        token: string
     }
 }
 
@@ -29,13 +29,47 @@ export async function loadConfig() {
         }
     } catch (e) { }
 
+    let envs = Object.keys(process.env).filter(k => k.startsWith('VERSION_CHECKER_'));
+
+    let objs = envs.map(k => createObject(k));
+    for(let o of objs){
+        config = mergeObject(config, o);
+    }
     recurseLoad(config, 'version_checker');
+    process.exit(1);
+}
+
+function mergeObject(o1, o2){
+    for(let key of Object.getOwnPropertyNames(o2)){
+        if(typeof o2[key] === 'object' && o2[key] !== null){
+            o1[key] = mergeObject(o1[key] || {}, o2[key]);
+        }else{
+            o1[key] = o1[key] || o2[key];
+        }
+    }
+    return o1;
+}
+
+function createObject(envKey){
+    let keys = envKey.replace('VERSION_CHECKER_', '').split('_').map(k => k.toLowerCase());
+    let obj = null;
+    for(let key of keys.reverse()){
+        if (obj === null) {
+            obj = {};
+            obj[key] = null;
+        }else{
+            let old = obj;
+            obj = {}
+            obj[key] = old;
+        }
+    }
+    return obj;
 }
 
 function recurseLoad(obj, path) {
-    for (let key in obj) {
+    for (let key of Object.getOwnPropertyNames(obj)) {
         let envKey = `${path}_${key}`;
-        if (typeof obj[key] === 'object') {
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
             recurseLoad(obj[key], envKey);
         } else {
             if (process.env[envKey.toUpperCase()]) {
