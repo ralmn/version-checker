8266bbbd35c5ecd8a7828d55cfb70cb5e3adf1fd
Mathieu ralmn HIREL <contact.ralmn@gmail.com>
2021-11-27 18:00:26 +0100

    Fix telemetry syntaxt

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -25,59 +25,59 @@ let tracerProvider = new NodeTracerProvider({
 
 if (process.env.TRACING_LOG === 'debug') {
     diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+}
+registerInstrumentations({
+    tracerProvider: tracerProvider,
+    instrumentations: [
+        (getNodeAutoInstrumentations() as any),
+        new TypeormInstrumentation(),
+        // {
+        //     plugins: {
+        //         express: {
+        //             enabled: true,
+        //             path: '@opentelemetry/plugin-express',
+        //         }
+        //     }
+        // },
+        new ExpressInstrumentation(),
+        new HttpInstrumentation()
+    ]
+});
 
-    registerInstrumentations({
-        tracerProvider: tracerProvider,
-        instrumentations: [
-            (getNodeAutoInstrumentations() as any),
-            new TypeormInstrumentation(),
-            // {
-            //     plugins: {
-            //         express: {
-            //             enabled: true,
-            //             path: '@opentelemetry/plugin-express',
-            //         }
-            //     }
-            // },
-            new ExpressInstrumentation(),
-            new HttpInstrumentation()
-        ]
-    });
 
-
-    if (process.env.JAEGER_ENDPOINT) {
-        const options = {
-            tags: [], // optional
-            endpoint: process.env.JAEGER_ENDPOINT, // 'http://localhost:14268/api/traces'
-            maxPacketSize: 65000 // optional
-        }
-        let traceExporter = new JaegerExporter(options);
-        tracerProvider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
+if (process.env.JAEGER_ENDPOINT) {
+    const options = {
+        tags: [], // optional
+        endpoint: process.env.JAEGER_ENDPOINT, // 'http://localhost:14268/api/traces'
+        maxPacketSize: 65000 // optional
     }
-    if (process.env.OTLP_HTTP_URL) {
-        const collectorOptions = {
-            url: process.env.OTLP_HTTP_URL, // url is optional and can be omitted - default is http://localhost:55681/v1/traces
-            headers: {}, // an optional object containing custom headers to be sent with each request
-            concurrencyLimit: 10, // an optional limit on pending requests
-        };
-        let traceExporter = new OTLPTraceExporter(collectorOptions);
-        tracerProvider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
-    }
-
-
-    tracerProvider.register({
-        propagator: new JaegerPropagator()
-    });
-
-
-    export const tracer = opentelemetry.trace.getTracer("version-checker");
-
-    export const addTraceId = (req, res, next) => {
-        const spanContext = getSpanContext(context.active());
-        if (spanContext) {
-            req.traceId = spanContext.traceId;
-            res.setHeader('vc-trace-id', spanContext.traceId)
-            console.log(`path=${req.path} traceId=${spanContext.traceId}`);
-        }
-        next();
+    let traceExporter = new JaegerExporter(options);
+    tracerProvider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
+}
+if (process.env.OTLP_HTTP_URL) {
+    const collectorOptions = {
+        url: process.env.OTLP_HTTP_URL, // url is optional and can be omitted - default is http://localhost:55681/v1/traces
+        headers: {}, // an optional object containing custom headers to be sent with each request
+        concurrencyLimit: 10, // an optional limit on pending requests
     };
+    let traceExporter = new OTLPTraceExporter(collectorOptions);
+    tracerProvider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
+}
+
+
+tracerProvider.register({
+    propagator: new JaegerPropagator()
+});
+
+
+export const tracer = opentelemetry.trace.getTracer("version-checker");
+
+export const addTraceId = (req, res, next) => {
+    const spanContext = getSpanContext(context.active());
+    if (spanContext) {
+        req.traceId = spanContext.traceId;
+        res.setHeader('vc-trace-id', spanContext.traceId)
+        console.log(`path=${req.path} traceId=${spanContext.traceId}`);
+    }
+    next();
+};
